c650ec61d943c2cb2676dbf58da89dc5a202f3d1
aew2sbee <aew2sbee@gmail.com>
2026-02-02 20:43:29 +0900

    feat: 数値の評価に関するテストケースを追加

---
+++ tests/static/number.test.ts
@@ -0,0 +1,28 @@
+describe("数値の評価", () => {
+  test("0.1+0.2は0.3に等しくない", () => {
+    expect(0.1 + 0.2).not.toBe(0.3);
+  });
+
+  test("0.1+0.2は0.3に近い", () => {
+    expect(0.1 + 0.2).toBeCloseTo(0.3);
+  });
+
+  test("0.3より大きいかどうか", () => {
+    expect(0.1 + 0.2 > 0.3).toBe(true);
+    expect(0.1 + 0.2).toBeGreaterThan(0.3);
+  });
+
+  test("0.3以上かどうか", () => {
+    expect(0.1 + 0.2 >= 0.3).toBe(true);
+    expect(0.1 + 0.2).toBeGreaterThanOrEqual(0.3);
+  });
+  test("0.3より小さいかどうか", () => {
+    expect(0.1 + 0.2 < 0.3).toBe(true);
+    expect(0.1 + 0.2).toBeLessThan(0.3);
+  });
+
+  test("0.3以下かどうか", () => {
+    expect(0.1 + 0.2 <= 0.3).toBe(true);
+    expect(0.1 + 0.2).toBeLessThanOrEqual(0.3);
+  });
+});
