e4c0d833e499c16bb431de95b388ef6c84579c63
aew2sbee <aew2sbee@gmail.com>
2026-02-03 21:11:34 +0900

    test: jest.fn()のモック実装に関するテストを追加

--- tests/static/mock.test.ts
+++ tests/static/mock.test.ts
@@ -1,6 +1,15 @@
 describe("モックオブジェクトの評価", () => {
-  test("jest.fn()呼び出しを確認する", () => {
+  test("jest.fn().mockImplementationで掛け算が可能か確認する", () => {
     const mockFunction = jest.fn().mockImplementation((x: number) => x * 2);
     expect(mockFunction(2)).toBe(4);
+  });
+  test("jest.fn().mockImplementationOnceで呼び出し回数に応じた結果が返ってくるか確認する", () => {
+    const mockFunction = jest
+      .fn()
+      .mockImplementationOnce(() => "1回目")
+      .mockImplementationOnce(() => "2回目");
+    expect(mockFunction()).toBe("1回目");
+    expect(mockFunction()).toBe("2回目");
+    expect(mockFunction()).toBe(undefined);
   });
 });
