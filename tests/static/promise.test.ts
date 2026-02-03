5abfc1a41d6e900529827200f1569393332a25ce
Furuta Naoki <naoki.furuta@japan-systems.co.jp>
2026-02-03 15:06:01 +0900

    fix: Promiseのreject処理に関するテストを追加

--- tests/static/promise.test.ts
+++ tests/static/promise.test.ts
@@ -1,10 +1,15 @@
 const MESSAGE = "success";
+const ERROR_MESSAGE = "error";
+const TIMEOUT = 100;
 
 const fetchDataWithPromiseResolve = () =>
   new Promise<string>((resolve) => {
-    setTimeout(() => {
-      resolve(MESSAGE);
-    }, 1000);
+    setTimeout(resolve, TIMEOUT, MESSAGE);
+  });
+
+const fetchDataWithPromiseReject = () =>
+  new Promise<string>((resolve, reject) => {
+    setTimeout(reject, TIMEOUT, ERROR_MESSAGE);
   });
 
 describe("Promiseの評価", () => {
@@ -22,5 +27,23 @@ describe("Promiseの評価", () => {
   test("async/awaitでsuccessで受け取る", async () => {
     const data = await fetchDataWithPromiseResolve();
     expect(data).toBe(MESSAGE);
+  });
+  test("rejectでerrorで受け取る", () => {
+    return fetchDataWithPromiseReject().catch((error) => {
+      expect(error).toBe(ERROR_MESSAGE);
+    });
+  });
+  test("doneを使ってrejectでerrorで受け取る", (done) => {
+    fetchDataWithPromiseReject().catch((error) => {
+      expect(error).toBe(ERROR_MESSAGE);
+      done(); // テストの終了を宣言
+    });
+  });
+  test("async/awaitでerrorで受け取る", async () => {
+    try {
+      await fetchDataWithPromiseReject();
+    } catch (error) {
+      expect(error).toBe(ERROR_MESSAGE);
+    }
   });
 });
